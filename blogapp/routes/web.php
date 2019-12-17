97037eb7938e36dd2a223f848972cc16ca262a6a
root <root@localhost.localdomain>
2019-12-17 17:21:38 +0000

    added Route and Route prefix

--- blogapp/routes/web.php
+++ blogapp/routes/web.php
@@ -19,6 +19,29 @@ Route::get('/', function () {
 ### we can route with this web.php
 ### if you use the post request then you have to use the 
 ### post request
+## this function will return the hello world
 Route::get('/hello',function(){
     return "hello world";
+});
+
+Route::get('/about',function(){
+    ## we can use the view for the sending template
+    return view('about');
+});
+
+
+## sending request with inside the 
+## the pages directory
+#Route::get('/contact',function(){
+#    return view('pages.contact');
+#});
+
+### setting up the prefix
+
+Route::prefix('mydomain')->group(function(){
+
+Route::get('/contact',function(){
+    return view('pages.contact');
+});
+
 });
